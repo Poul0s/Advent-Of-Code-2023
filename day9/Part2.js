9033281e66bf2f4b475804b460c027bf1a081983
Paul Salame <psalame@2C5.42angouleme.fr>
2023-12-09 11:32:38 +0100

    day 9

---
+++ day9/Part2.js
@@ -0,0 +1,61 @@
+const fs = require('fs');
+const fileName = process.argv.length > 2 ? process.argv[2] : "input.txt"
+
+Array.prototype.isFullZero = function() {
+    let res = true;
+    for (let i = 0; i < this.length; i++)
+    {
+        if (this[i] != 0)
+        {
+            res = false;
+            break ;
+        }
+    }
+    return (res);
+}
+
+Array.prototype.last = function() {
+    return (this[this.length - 1]);
+}
+
+Array.prototype.pushFirst = function(ele) {
+    return [ele].concat(this);
+}
+
+fs.readFile(fileName, {encoding: "utf-8"}, (err, res) => {
+    if (err)
+        console.error(err)
+    else
+    {
+        var historys = []
+        res.split('\n').forEach(line => {
+            historys.push([line.split(' ').filter(e => e != '').map(e => parseInt(e))]);
+        })
+        let sum = 0;
+        historys.forEach(historyArr => {
+            let lastArr = historyArr.last();
+            while (!lastArr.isFullZero())
+            {
+                let newArray = [];
+                for (let i = 0; i < lastArr.length - 1; i++)
+                    newArray.push(lastArr[i + 1] - lastArr[i]);
+                historyArr.push(newArray);
+                lastArr = newArray;
+            }
+            lastArr.push(0);
+            for (let i = historyArr.length - 2; i >= 0; i--)
+                historyArr[i] = historyArr[i].pushFirst(historyArr[i][0] - historyArr[i + 1][0]);
+            console.log(historyArr);
+            console.log("\n\n\n\n");
+            sum += historyArr[0][0];
+        })
+        console.log(sum);
+    }
+})
+
+
+
+function isFullZero(arr)
+{
+
+}
